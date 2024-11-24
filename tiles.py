153d0cae22f9e12d1a624a4d6f7244e274c618cb
Mathematriques <Mathematriques@users.noreply.github.com>
2024-11-24 15:36:44 +0100

    Add display object to tile.  - This should ease the generalisation to an interactive game

--- tiles.py
+++ tiles.py
@@ -1,54 +1,63 @@
+from colours import *
 from numpy import random
 
 
-class Prison:
+class Tile:
+    def __init__(self, displayer):
+        self.displayer = displayer
+
+
+class Prison(Tile):
     pass
 
 
-class Tax:
-    def __init__(self, value):
+class Tax(Tile):
+    def __init__(self, value, displayer):
+        Tile.__init__(self, displayer)
         self.value = value
 
-    def action(self, player):
+    def take_money_from(self, player):
         player.pay(self.value)
 
 
-class Purchasable:
-    def __init__(self, price):
+class Purchasable(Tile):
+    def __init__(self, price, displayer):
+        Tile.__init__(self, displayer)
         self.price = price
         self.rent = 0
         self.owner = None
 
-    def action(self, player):
+    def take_money_from(self, player):
         payed = player.pay(self.rent)
         self.owner.receive(payed)
 
 
 class Station(Purchasable):
-    def __init__(self):
-        super().__init__(200)
+    def __init__(self, displayer):
+        super().__init__(200, displayer)
 
     def update_rent(self, number_stations):
         self.rent = 25 * number_stations
 
 
 class Company(Purchasable):
-    def __init__(self):
-        super().__init__(150)
+    def __init__(self, displayer):
+        super().__init__(150, displayer)
 
     def update_rent(self, monopoly):
         # member rent works as a dice multiplier here
         self.rent = 10 if monopoly else 4
 
-    def action(self, player, dices):
+    def take_money_from(self, player, dices):
         payed = player.pay(self.rent * sum(dices))
         self.owner.receive(payed)
 
 
 class Coloured(Purchasable):  # Interface
-    def __init__(self, price, rents):
-        super().__init__(price)
-        super().properties.append(self)
+    def __init__(self, price, construction, rents, displayer):
+        super().__init__(price, displayer)
+        type(self).properties.append(self)
+        self.construction = construction
         self.rents = rents
 
     def update_rent(self, monopoly, developpement):
@@ -90,7 +99,7 @@ class Blue(Coloured):
     properties = []
 
 
-class Chance:
+class Chance(Tile):
     SORTIE_DE_PRISON = 0
     AVANCEZ_DEPART = 1
     ALLEZ_EN_PRISON = 2
@@ -112,7 +121,7 @@ class Chance:
     random.shuffle(deck)
 
 
-class Caisse:
+class Caisse(Tile):
     SORTIE_DE_PRISON = 0
     AVANCEZ_DEPART = 1
     ALLEZ_EN_PRISON = 2
