b25f2ce98895663cf84c7cffd23341ed3c8f3e67
Mathematriques <Mathematriques@users.noreply.github.com>
2024-11-30 23:44:21 +0100

    Add Chance and Caisse tiles.  - rework move and position  - add prison turn  - add buying, construction and rent of tiles

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tiles import *
 from colours import *
+
 from itertools import cycle
+from collections import defaultdict
+
 from numpy import random
 
 class PlayerDebugDisplayer:
@@ -8,22 +11,31 @@ class PlayerDebugDisplayer:
         self.name = name
 
     def __call__(self, player):
-        print(f"{self.name} est en {player.position}")
+        print(f"Tour de {self.name} qui a {colours.fg.yellow}{player.money}€{colours.reset}")
+        for group in player.wallet:
+            print(f"\t{group.__name__} : {", ".join(t.displayer.name for t in player.wallet[group])}")
 
-    def pay(self, amount):
-        print(f"\t{self.name} donne {colours.fg.yellow}{amount}€{colours.reset}")
+    def lose(self, amount):
+        print(f"\t\t{self.name} donne {colours.fg.yellow}{amount}€{colours.reset}")
 
     def win(self, amount):
-        print(f"\t{self.name} reçoit {colours.fg.yellow}{amount}€{colours.reset}")
+        print(f"\t\t{self.name} reçoit {colours.fg.yellow}{amount}€{colours.reset}")
 
-    def go(self, position):
-        print(f"{self.name} va en {position}")
+    def pay(self, other, amount):
+        print(f"\t\t{self.name} −−{colours.fg.yellow}{amount}€{colours.reset}−−> {other.displayer.name}")
+
+    def step(self):
+        return
+        print(f"{self.name} avance d'une case")
 
     def go_to_prison(self):
         print(f"{colours.bg.red}{colours.fg.black}{self.name} va en Prison !{colours.reset}")
 
     def leave_prison(self):
-        print(f"{colours.bg.gree}{self.name} va en Prison !{colours.reset}")
+        print(f"{colours.bg.green}{colours.fg.black}{self.name} Sort de Prison !{colours.reset}")
+
+    def in_prison(self):
+        print(f"Tour de {self.name} qui a {colours.fg.yellow}{player.money}€{colours.reset} est en Prison")
 
 
 class TileDebugDisplayer:
@@ -32,25 +44,64 @@ class TileDebugDisplayer:
         self.colour = colour
 
     def __call__(self):
-        print(f"Sur la case {self.colour}{self.name}{colours.reset}")
+        print(f"\tSur la case {self.colour}{self.name}{colours.reset}")
+
+
+class ChanceDebugDisplayer:
+    def __call__(self):
+        print(f"\tSur la case {colours.bg.red}Chance{colours.reset}")
+
+    def effect(self, description):
+        print(f"\t\t{colours.bold}{description}{colours.reset}")
+
+
+class CaisseDebugDisplayer:
+    def __call__(self):
+        print(f"\tSur la case {colours.bg.blue}Caisse{colours.reset}")
+
+    def effect(self, description):
+        print(f"\t\t{colours.bold}{description}{colours.reset}")
 
 
 class Player:
     def __init__(self, displayer):
         self.displayer = displayer
         self.money = 1500
-        self._go(Plateau.DEPART)
+        self.position = Tile.DEPART
         self.doubles_in_a_row = 0
         self.turns_in_prison = 0
+        self.chance_out = False
+        self.caisse_out = False
+        self.wallet = defaultdict(set)
 
-    def pay(self, amount):
+    # Subclassing
+    def prison_will(self):
+        return False, False, False
+
+    # Subclassing
+    def veut_acheter(self, purchasable):
+        return True
+
+    # Subclassing
+    def vent_construire(self, groups):
+        for _ in range(5):
+            pass
+
+    def lose(self, amount):
         self.money -= amount
-        self.displayer.pay(amount)
-        print("Add player end")
+        self.displayer.lose(amount)
+        print("\tAdd player end + real amount payed")
+        return amount
 
     def win(self, amount):
         self.money += amount
         self.displayer.win(amount)
+
+    def pay(self, other, amount):
+        other.money += amount
+        self.money -= amount
+        self.displayer.pay(other, amount)
+        print("\tAdd player end + real amount payed")
 
     def too_many_doubles(self, dice1, dice2):
         double = dice1 == dice2
@@ -60,37 +111,68 @@ class Player:
             return True
         return False
 
-    def _go(self, position):
-        self.displayer.go(position)
-        self.position = position
-
-    def forward(self, position):
-        while self.position != (position % Plateau.TOTAL):
-            self._go((self.position + 1) % Plateau.TOTAL)
-            if self.position == Plateau.DEPART:
+    def move_to(self, position, step=1):
+        assert abs(step) == 1
+        while self.position != position:
+            self.displayer.step()
+            self.position += step
+            # Technically DÉPART can be elsewhere than zero
+            if self.position in (-1, Tile.TOTAL):
+                self.position = 0
+            if self.position == Tile.DEPART:
                 self.win(200)
+
+    def move_from_dice(self, dice1, dice2):
+        self.move_to((self.position + dice1 + dice2) % Tile.TOTAL)
 
     def go_to_prison(self):
         self.displayer.go_to_prison()
-        self._go(Plateau.PRISON)
+        self.position = Tile.PRISON
         self.turns_in_prison = 0
 
     def leave_prison(self):
         self.displayer.leave_prison()
-        self._go(Plateau.SIMPLE_VISITE)
+        self.position = Tile.PRISON
         self.turns_in_prison = 0
 
+    def tax_construction(self, house, hotel):
+        print("\tAdd tax_construction later")
 
-class Plateau:
-    DEPART = 0
-    SIMPLE_VISITE = 10
-    PRISON = 30
-    TOTAL = 40
+    def in_prison_and_stay(self, dice1, dice2, board):
+        if self.turns_in_prison == 1:
+            chance, caisse, taxe = self.prison_will()
+            if chance and self.chance_out:
+                board.chance_deck.append(Chance.SORTIE_DE_PRISON)
+            elif caisse and self.caisse_out:
+                board.caisse_deck.append(Caisse.SORTIE_DE_PRISON)
+            elif taxe and (self.money >= 50):
+                self.lose(50)
+            elif dice1 != dice2:
+                # Look at dices output only if no othe method is wanted
+                return True # stay in prison
+        
+        elif self.turns_in_prison < 3:
+            if dice1 != dice2:
+                return True # stay in prison
 
+        player.leave_prison()
+        return False
+
+    def buy(self, tile):
+        self.lose(tile.price)
+        group = type(tile)
+        self.wallet[group].add(tile)
+        tile.owner = self
+        tile.buy_update_rent(self.wallet[group])
+
+
+class Board:
     def __init__(self, players):
         self.players = cycle(players)
+        self.chance_deck = Chance.make_deck()
+        self.caisse_deck = Caisse.make_deck()
         self.tiles = [
-            # fmt: off
+            # fmt: on
             Tile(
                 TileDebugDisplayer("Départ", colours.bold)
             ),
@@ -98,7 +180,8 @@ class Plateau:
                 TileDebugDisplayer("Boulevard de Belleville", colours.fg.brown)
             ),
             Caisse(
-                TileDebugDisplayer("Caisse de Communauté", colours.bg.blue)
+                self.caisse_deck,
+                CaisseDebugDisplayer()
             ),
             Brown(60, 50, (4, 20, 60, 180, 320, 450), 
                 TileDebugDisplayer("Rue Lecourbe", colours.fg.brown)
@@ -113,7 +196,8 @@ class Plateau:
                 TileDebugDisplayer("Rue Vaugirard", colours.fg.lightgrey)
             ),
             Chance(
-                TileDebugDisplayer("Chance", colours.bg.red)
+                self.chance_deck,
+                ChanceDebugDisplayer()
             ),
             Grey(100, 50, (6, 30, 90, 270, 400, 550), 
                 TileDebugDisplayer("Rue de Courcelles", colours.fg.lightgrey)
@@ -143,7 +227,8 @@ class Plateau:
                 TileDebugDisplayer("Avenue Mozart", colours.fg.brown)
             ),
             Caisse(
-                TileDebugDisplayer("Caisse de Communauté", colours.bg.blue)
+                self.caisse_deck,
+                CaisseDebugDisplayer()
             ),
             Orange(180, 100, (14, 70, 200, 550, 750, 950), 
                 TileDebugDisplayer("Boulevard Saint-Michel", colours.fg.brown)
@@ -158,7 +243,8 @@ class Plateau:
                 TileDebugDisplayer("Avenue Matignon", colours.fg.red)
             ),
             Chance(
-                TileDebugDisplayer("Chance", colours.bg.red)
+                self.chance_deck,
+                ChanceDebugDisplayer()
             ),
             Red(220, 150, (18, 90, 250, 700, 875, 1050), 
                 TileDebugDisplayer("Boulevard Malesherbes", colours.fg.red)
@@ -191,7 +277,8 @@ class Plateau:
                 TileDebugDisplayer("Avenue Foch", colours.fg.green)
             ),
             Caisse(
-                TileDebugDisplayer("Caisse de Communauté", colours.bg.blue)
+                self.caisse_deck,
+                CaisseDebugDisplayer()
             ),
             Green(320, 200, (28, 150, 450, 1000, 1200, 1400), 
                 TileDebugDisplayer("Boulevard des Capucines", colours.fg.green)
@@ -200,7 +287,8 @@ class Plateau:
                 TileDebugDisplayer("Gare Saint-Lazare", colours.fg.darkgrey)
             ),
             Chance(
-                TileDebugDisplayer("Chance", colours.bg.red)
+                self.chance_deck,
+                ChanceDebugDisplayer()
             ),
             Blue(350, 200, (35, 175, 500, 1100, 1300, 1500), 
                 TileDebugDisplayer("Avenue des Champs-Élysées", colours.fg.blue)
@@ -214,37 +302,56 @@ class Plateau:
             # fmt: on
         ]
 
+        # TODO : Dirty monkey patching
+        groups = defaultdict(set)
+        for t in self.tiles:
+            groups[type(t)].add(t)
 
-plateau = Plateau(
-    [Player(PlayerDebugDisplayer("Toto")), Player(PlayerDebugDisplayer("Tata"))]
-)
+        for t in self.tiles:
+            t.group = groups[type(t)]
+
+
+players = [Player(PlayerDebugDisplayer("Toto")), Player(PlayerDebugDisplayer("Tata"))]
+board = Board(players)
+
 for _ in range(100):
-    player = next(plateau.players)
-    player.displayer(player)
-
-    if player.position == Plateau.PRISON:
-        print("Ajouter les tours en prison")
-        continue
-
+    player = next(board.players)
     dice1, dice2 = random.randint(1, 7, 2)
-    if player.too_many_doubles(dice1, dice2):
+
+    # Special cases
+    if player.position == Tile.PRISON:
+        stay = player.in_prison_and_stay(dice1, dice2, board)
+        if stay:
+            continue
+    elif player.too_many_doubles(dice1, dice2):
         player.go_to_prison()
         continue
 
-    player.forward(player.position + dice1 + dice2)
-
-    tile = plateau.tiles[player.position]
+    # Normal move
+    player.displayer(player)
+    player.move_from_dice(dice1, dice2)
+    tile = board.tiles[player.position]
     tile.displayer()
-    if type(tile) is Tile:
-        print("Rien de spécial")
-    elif isinstance(tile, Prison):
+
+    double = False
+
+    if isinstance(tile, Prison):
         player.go_to_prison()
         continue
-    elif isinstance(tile, Purchasable) and tile.owner is None:
-        print("Ajouter le mechanisme d'achat")
-    elif isinstance(tile, Caisse):
-        print("Faire la misère au joueur")
-    elif isinstance(tile, Chance):
-        print("Faire la misère au joueur")
-    else:
-        tile.take_money_from(player)
+    elif isinstance(tile, Tax):
+        tile.tax_from(player)
+    elif isinstance(tile, Chance) or isinstance(tile, Caisse):
+        moved, double = tile.action(player, players)
+        if moved:
+            if player.position == Tile.PRISON:
+                player.go_to_prison()
+                continue
+            tile = board.tiles[player.position]
+            tile.displayer()
+    if isinstance(tile, Purchasable):
+        if tile.owner:
+            tile.rent_from(player, double, dice1, dice2)
+        elif player.veut_acheter(tile) and player.money >= tile.price:
+            player.buy(tile)
+
+    print("Add constructions")
--- tiles.py
+++ tiles.py
@@ -3,6 +3,37 @@ from numpy import random
 
 
 class Tile:
+    # Shortcuts TODO : make those consistent with the plateau list of tiles
+    DEPART = 0
+    IMPOTS_SUR_LE_REVENU = 4
+    GARE_MONTPARNASSE = 5
+    SIMPLE_VISITE = 10
+    BOULEVARD_DE_LA_VILLETTE = 11
+    COMPAGNIE_D_ELECTRICITE = 12
+    GARE_DE_LYON = 15
+    AVENUE_HENRI_MARTIN = 24
+    GARE_DU_NORD = 25
+    COMPAGNIE_D_EAU = 28
+    PRISON = 30
+    GARE_SAINT_LAZARE = 35
+    TAXE_DE_LUXE = 38
+    RUE_DE_LA_PAIX = 39
+    TOTAL = 40
+
+    def closest_station(position):
+        if Tile.GARE_MONTPARNASSE <= position < Tile.GARE_DE_LYON:
+            return Tile.GARE_DE_LYON
+        elif Tile.GARE_DE_LYON <= position < Tile.GARE_DU_NORD:
+            return Tile.GARE_DU_NORD
+        elif Tile.GARE_DU_NORD <= position < Tile.GARE_SAINT_LAZARE:
+            return Tile.GARE_SAINT_LAZARE
+        return Tile.GARE_MONTPARNASSE
+
+    def closest_company(position):
+        if Tile.COMPAGNIE_D_ELECTRICITE <= position < Tile.COMPAGNIE_D_EAU:
+            return Tile.COMPAGNIE_D_EAU
+        return Tile.COMPAGNIE_D_ELECTRICITE
+
     def __init__(self, displayer):
         self.displayer = displayer
 
@@ -16,88 +47,86 @@ class Tax(Tile):
         Tile.__init__(self, displayer)
         self.value = value
 
-    def take_money_from(self, player):
-        player.pay(self.value)
+    def tax_from(self, player):
+        player.lose(self.value)
 
 
 class Purchasable(Tile):
     def __init__(self, price, displayer):
         Tile.__init__(self, displayer)
         self.price = price
-        self.rent = 0
         self.owner = None
 
-    def take_money_from(self, player):
-        payed = player.pay(self.rent)
-        self.owner.receive(payed)
+    def rent_from(self, player, double, dice1, dice2):
+        rent = self.compute_rent(double, dice1, dice2)
+        payed = player.lose(rent)
+        self.owner.win(payed)
 
 
 class Station(Purchasable):
     def __init__(self, displayer):
         super().__init__(200, displayer)
 
-    def update_rent(self, number_stations):
-        self.rent = 25 * number_stations
+    def buy_update_rent(self, wallet_group):
+        rent = 25 * len(wallet_group)
+        for t in wallet_group:
+            t.compute_rent = lambda double, dice1, dice2 : rent
 
 
 class Company(Purchasable):
     def __init__(self, displayer):
         super().__init__(150, displayer)
 
-    def update_rent(self, monopoly):
-        # member rent works as a dice multiplier here
-        self.rent = 10 if monopoly else 4
-
-    def take_money_from(self, player, dices):
-        payed = player.pay(self.rent * sum(dices))
-        self.owner.receive(payed)
+    def buy_update_rent(self, wallet_group):
+        monopoly = wallet_group == self.group
+        multiplier = 10 if monopoly else 4
+        for t in wallet_group:
+            t.compute_rent = lambda double, dice1, dice2 : multiplier * (dice1 + dice2)
 
 
-class Coloured(Purchasable):  # Interface
+class Coloured(Purchasable):
+    HOTEL = 5  # Pas de crise du logement pris en compte
+
     def __init__(self, price, construction, rents, displayer):
         super().__init__(price, displayer)
-        type(self).properties.append(self)
         self.construction = construction
         self.rents = rents
 
-    def update_rent(self, monopoly, developpement):
-        if developpement == 0 and not monopoly:
-            self.rent = 2 * self.rents[0]
-        else:
-            self.rent = self.rents[developpement]
+    def buy_update_rent(self, wallet_group):
+        monopoly = wallet_group == self.group
+        for t in wallet_group:
+            t.compute_rent = lambda double, dice1, dice2 : t.rents[0] * (1 + monopoly)
 
+    def construct_update_rent(self, wallet_group):
+        assert wallet_group == self.group
+        assert self.construction < Coloured.HOTEL
+        self.construction += 1
+        for t in wallet_group:
+            t.compute_rent = lambda double, dice1, dice2 : t.rents[t.construction]
 
 class Brown(Coloured):
-    properties = []
-
+    pass
 
 class Grey(Coloured):
-    properties = []
-
+    pass
 
 class Pink(Coloured):
-    properties = []
-
+    pass
 
 class Orange(Coloured):
-    properties = []
-
+    pass
 
 class Red(Coloured):
-    properties = []
-
+    pass
 
 class Yellow(Coloured):
-    properties = []
-
+    pass
 
 class Green(Coloured):
-    properties = []
-
+    pass
 
 class Blue(Coloured):
-    properties = []
-
+    pass
 
 class Chance(Tile):
     SORTIE_DE_PRISON = 0
@@ -117,8 +146,77 @@ class Chance(Tile):
     COMPAGNIE_PLUS_PROCHE = 15
     TOTAL = 16
 
-    deck = list(range(TOTAL))
-    random.shuffle(deck)
+    def __init__(self, deck, displayer):
+        super().__init__(displayer)
+        self.deck = deck
+
+    def make_deck():
+        deck = list(range(Chance.TOTAL))
+        random.shuffle(deck)
+        return deck
+
+    def action(self, player, players):
+        card = self.deck.pop(0)  # enlève la card du sommet du deck
+
+        if card == Chance.SORTIE_DE_PRISON:
+            self.displayer.effect("Vous êtes libéré de prison.")
+            player.chance_out = True  # ajoute à la main du player
+            return False, False # not moved, not doubling
+
+        self.deck.append(card) # repose la card dans le deck
+        if card == Chance.AVANCEZ_DEPART:
+            self.displayer.effect("Avancez jusqu'à la case départ")
+            player.move_to(Tile.DEPART)
+            return True, False # moved, not doubling
+        elif card == Chance.ALLEZ_EN_PRISON:
+            self.displayer.effect("Allez en prison !")
+            player.move_to(Tile.PRISON)
+            return True, False # moved, not doubling
+        elif card == Chance.DIVIDENDE:
+            self.displayer.effect("Recevez 50€ de dividendes")
+            player.win(50)
+        elif card == Chance.IMMEUBLE:
+            self.displayer.effect("Vos placements immobiliers vous rapportent 150€")
+            player.win(150)
+        elif card == Chance.EXCES_DE_VITESSE:
+            self.displayer.effect("Excès de vitesse : payez 15€")
+            player.lose(15)
+        elif card == Chance.PRESIDENT:
+            self.displayer.effect("Vous êtes président : payez 50€ à chaque players")
+            for j in players:
+                player.pay(j, 50)
+        elif card == Chance.REPARATION:
+            self.displayer.effect("Frais de réparations : payez 25€ par maison et 100€ par hôtel")
+            player.tax_construction(25, 100)
+        elif card == Chance.RUE_DE_LA_PAIX:
+            self.displayer.effect("Avancez rue de la Paix")
+            player.move_to(Tile.RUE_DE_LA_PAIX)
+            return True, False # moved, not doubling
+        elif card == Chance.AVANCEZ_BOULEVARD_DE_LA_VILLETTE:
+            self.displayer.effect("Avancez Boulevard de la Villette")
+            player.move_to(Tile.BOULEVARD_DE_LA_VILLETTE)
+            return True, False # moved, not doubling
+        elif card == Chance.RENDEZ_VOUS_AVENUE_HENRI_MARTIN:
+            self.displayer.effect("Rendez vous avenue Henri-Martin")
+            player.move_to(Tile.AVENUE_HENRI_MARTIN)
+            return True, False # moved, not doubling
+        elif card == Chance.ALLEZ_GARE_MONTPARNASSE:
+            self.displayer.effect("Avancez jusqu'à la gare Montparnasse")
+            player.move_to(Tile.GARE_MONTPARNASSE)
+            return True, False # moved, not doubling
+        elif card == Chance.RECULEZ_DE_TROIS_CASES:
+            self.displayer.effect("Reculez de 3 cases")
+            player.move_to((player.position - 3) % Tile.TOTAL, step=-1)
+            return True, False # moved, not doubling
+        elif card in Chance.GARE_PLUS_PROCHE:
+            self.displayer.effect("Allez à la gare la plus proche. Payer le loyer double")
+            player.move_to(Tile.closest_station(player.position))
+            return True, True # doubling, moved
+        elif card == Chance.COMPAGNIE_PLUS_PROCHE:
+            self.displayer.effect("Allez à la compagnie la plus proche. Payer le loyer double")
+            player.move_to(Tile.closest_company(player.position))
+            return True, True # doubling, moved
+        return False, False # not moved, not doubling
 
 
 class Caisse(Tile):
@@ -140,5 +238,70 @@ class Caisse(Tile):
     TRAVAUX = 15
     TOTAL = 16
 
-    deck = list(range(TOTAL))
-    random.shuffle(deck)
+    def __init__(self, deck, displayer):
+        super().__init__(displayer)
+        self.deck = deck
+
+    def make_deck():
+        deck = list(range(Caisse.TOTAL))
+        random.shuffle(deck)
+        return deck
+
+    def action(self, player, players):
+        card = self.deck.pop(0)  # enlève la card du sommet du deck
+
+        if card == Caisse.SORTIE_DE_PRISON:
+            self.displayer.effect("Vous êtes libéré de prison.")
+            player.caisse_out = True  # ajoute à la main du player
+            return False, False # not moved, not doubling
+
+        self.deck.append(card) # repose la card dans le deck
+        if card == Caisse.AVANCEZ_DEPART:
+            self.displayer.effect("Avancez jusqu'à la case départ")
+            player.move_to(Tile.DEPART)
+            return True, False # moved, not doubling
+        elif card == Caisse.ALLEZ_EN_PRISON:
+            self.displayer.effect("Allez en prison !")
+            player.move_to(Tile.PRISON)
+            return True, False # moved, not doubling
+        elif card == Caisse.ASSURANCE_VIE:
+            self.displayer.effect("Votre assurance vie vous rapporte 100€")
+            player.win(100)
+        elif card == Caisse.IMPOTS:
+            self.displayer.effect("Erreur des impôts en votre faveur. Recevez 20€")
+            player.win(20)
+        elif card == Caisse.PLACEMENT:
+            self.displayer.effect("Vos placement vous rapportent 100€")
+            player.win(100)
+        elif card == Caisse.HERITAGE:
+            self.displayer.effect("Vous héritez de 100€")
+            player.win(100)
+        elif card == Caisse.BANQUE:
+            self.displayer.effect("Erreur de la banque en votre faveur. Recevez 200€")
+            player.win(200)
+        elif card == Caisse.BEAUTE:
+            self.displayer.effect("Vous remportez le prix de beauté. Recevez 10€")
+            player.win(10)
+        elif card == Caisse.EXPERT:
+            self.displayer.effect("Expert ? Recevez 25€")
+            player.win(25)
+        elif card == Caisse.STOCK:
+            self.displayer.effect("Vos actions vous rapportent 50€")
+            player.win(50)
+        elif card == Caisse.HOSPITALISATION:
+            self.displayer.effect("Payez 100€ de fraix d'hospitalisation")
+            player.lose(100)
+        elif card == Caisse.MEDECIN:
+            self.displayer.effect("Payez 50€ de fraix de médecin")
+            player.lose(50)
+        elif card == Caisse.SCOLARITE:
+            self.displayer.effect("Payez 50 € de fraix de scolarité")
+            player.lose(50)
+        elif card == Caisse.ANNIVERSAIRE:
+            self.displayer.effect("C'est votre anniversaire. Recevez 10€ de la part de chaque player")
+            for j in players:
+                j.pay(player, 10)
+        elif card == Caisse.TRAVAUX:
+            self.displayer.effect("Travaux : payez 40€ par maison et 115€ par hôtel")
+            player.tax_construction(40, 115)
+        return False, False # not moved, not doubling
